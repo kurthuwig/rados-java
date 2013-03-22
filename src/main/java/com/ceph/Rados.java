5a9b4af8c6c3f305a638783c22654cef670a9974
Wido den Hollander <wido@widodh.nl>
2013-03-22 10:56:52 +0100

    Add Java docs

--- src/main/java/com/ceph/Rados.java
+++ src/main/java/com/ceph/Rados.java
@@ -11,16 +11,33 @@ public class Rados {
 
     protected Pointer clusterPtr;
 
+    /**
+     * Construct a RADOS Object which invokes rados_create
+     *
+     * @param id
+     *            the cephx id to authenticate with
+     */
     public Rados(String id) {
         this.clusterPtr = new Memory(Pointer.SIZE);
         rados.rados_create(this.clusterPtr, id);
     }
 
+    /**
+     * Construct a RADOS Object which invokes rados_create
+     *
+     */
     public Rados() {
         this.clusterPtr = new Memory(Pointer.SIZE);
         rados.rados_create(this.clusterPtr, "");
     }
 
+    /**
+     * Read a Ceph configuration file
+     *
+     * @param file
+     *            A file object with the path to a ceph.conf
+     * @throws RadosException
+     */
     public void confReadFile(File file) throws RadosException {
         int r = rados.rados_conf_read_file(this.clusterPtr.getPointer(0), file.getAbsolutePath());
         if (r < 0) {
@@ -28,6 +45,15 @@ public class Rados {
         }
     }
 
+    /**
+     * Set a RADOS configuration option
+     *
+     * @param option
+     *            the name of the option
+     * @param value
+     *            the value configuration value
+     * @throws RadosException
+     */
     public void confSet(String option, String value) throws RadosException {
         int r = rados.rados_conf_set(this.clusterPtr.getPointer(0), option, value);
         if (r < 0) {
@@ -35,6 +61,11 @@ public class Rados {
         }
     }
 
+    /**
+     * Connect to the Ceph cluster
+     *
+     * @throws RadosException
+     */
     public void connect() throws RadosException {
         int r = rados.rados_connect(this.clusterPtr.getPointer(0));
         if (r < 0) {
@@ -42,6 +73,13 @@ public class Rados {
         }
     }
 
+    /**
+     * Create a RADOS pool
+     *
+     * @param name
+     *            the name of the pool to be created
+     * @throws RadosException
+     */
     public void poolCreate(String name) {
         rados.rados_pool_create(this.clusterPtr.getPointer(0), name);
     }
@@ -51,6 +89,11 @@ public class Rados {
         super.finalize();
     }
 
+    /**
+     * Get the librados version
+     *
+     * @return a int array with the minor, major and extra version
+     */
     public int[] getVersion() {
         IntByReference minor = new IntByReference();
         IntByReference major = new IntByReference();
