4e181bc740f33d4a8c55419713c8ae960d55e249
Wido den Hollander <wido@widodh.nl>
2013-03-29 10:00:38 +0100

    Add the RadosObjectInfo file
    Forgot this one in the last commit

---
+++ src/main/java/com/ceph/jna/RadosObjectInfo.java
@@ -0,0 +1,51 @@
+/*
+ * RADOS Java - Java bindings for librados
+ *
+ * Copyright (C) 2013 Wido den Hollander <wido@42on.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file LICENSE.
+ *
+ */
+
+package com.ceph.jna;
+
+public class RadosObjectInfo {
+
+    private String oid;
+    private long size;
+    private long mtime;
+
+    public RadosObjectInfo(String oid, long size, long mtime) {
+        this.oid = oid;
+        this.size = size;
+        this.mtime = mtime;
+    }
+
+    /**
+     * Return the object name
+     * @return String
+    */
+    public String getOid() {
+        return this.oid;
+    }
+
+    /**
+     * Returns the size in bytes
+     * @return long
+     */
+    public long getSize() {
+        return this.size;
+    }
+
+    /**
+     * Returns the modification time
+     * @return long
+     */
+    public long getMtime() {
+        return this.mtime;
+    }
+
+}
